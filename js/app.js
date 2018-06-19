458227875c0abbfe45c334bcba8e01dd694bd3a4
Jaffacake <jaffacakestudio@gmail.com>
2018-06-18 23:12:32 -0500

    Javascript session
    Removed Previous Code
    Added new JS that will add tasks to todo list

--- js/app.js
+++ js/app.js
@@ -1,34 +1,21 @@
-//Remove and Complete Image or checkbox
+
+var add = document.getElementById('add')
+    add.addEventListener('click', function (event) {
+    var value = document.querySelector('input').value;
+    event.preventDefault();
+    if (value) 
+        {addItemTodo(value)};
+    );
+    
+document.getElementById('item').value = '';
 
 
-//User clicked on the submit button
-//If there is any text in the newtask field add text it to the todolist
-document.getElementById('add').addEventListener('click', function() {
-    var value = document.getElementById('newTask').value;
-    if (value) addItemTodo(value);
-
-});
-
-
-//Adds a new ite to the todo list
-function addItemTodo(text) {
-    var list = document.getElementById('todo')
-
+function addItemTodo (text) {
     var item = document.createElement('li');
+    var list = document.getElementById('todo');
     item.innerText = text;
 
-    var buttons = document.createElement('div');
-    buttons.classList.add('button');
-
-    var remove = document.createElement('button');
-    remove.classList.add('remove');
-
-    var complete = document.createElement('button');
-
-
-    buttons.appendChild(remove);
-    buttons.appendChild(complete);
-    item.appendChild('buttons');
-
+    list.appendChild(item);
+    
 }
 
