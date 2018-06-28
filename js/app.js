b9759d4dc6077809ca503215b05fe63566353922
Poly Goveia <poly.goveia@gmail.com>
2018-06-28 14:07:12 -0700

    add delete button

--- js/app.js
+++ js/app.js
@@ -1,25 +1,29 @@
 
-let add = document.getElementById('add')
-    add.addEventListener('click', function (event) {
+const ul = document.querySelector('ul');
+const removeButton = document.getElementsByClassName('remove');
+const add = document.getElementById('add')
+
+add.addEventListener('click', function (event) {
     event.preventDefault();
     let input = document.querySelector('input');
     let inputValue = input.value;
-    input.value = '';
+    input.value = ''
     if (inputValue) {
         addItemTodo(inputValue)
-        
     };
     
     
 }); 
-    
-
 
 function addItemTodo (text) {
-    let item = document.createElement('li');
     let list = document.getElementById('todo');
+    let item = document.createElement('li');
+    let remove = document.createElement('button');
+    remove.className = 'remove';
     item.innerText = text;
 
     list.appendChild(item);
+    item.appendChild(remove);
 };
+
 
