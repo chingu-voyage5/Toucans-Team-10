8595a0bea5c5cb102efc1c04ecb6c1d6a582b1e4
Lauren Fosgett <lfosgett@asu.edu>
2018-07-08 12:36:02 -0700

    remove task function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 const ul = document.querySelector('ul');
 const removeButton = document.getElementsByClassName('remove');
 const add = document.getElementById('add')
@@ -11,19 +10,31 @@ add.addEventListener('click', function (event) {
     if (inputValue) {
         addItemTodo(inputValue)
     };
-    
-    
-}); 
 
-function addItemTodo (text) {
+
+});
+
+// remove item from list
+function removeItem(e) {
+    let item = this.parentNode;
+    let parent = item.parentNode;
+
+    parent.removeChild(item);
+
+};
+
+// add item to list
+function addItemTodo(text) {
     let list = document.getElementById('todo');
     let item = document.createElement('li');
     let remove = document.createElement('button');
     remove.className = 'remove';
     item.innerText = text;
 
+
+    //add event listener remove item
+    remove.addEventListener('click', removeItem);
+
     list.appendChild(item);
     item.appendChild(remove);
 };
-
-
