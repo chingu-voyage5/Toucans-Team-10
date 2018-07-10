1209c996eb7a446841d356f1cf0022d8589ff31f
Jaffacake <jaffacakestudio@gmail.com>
2018-07-09 21:56:29 -0500

    Complete Button
    Added complete button.
    Small styling done to buttons.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const ul = document.querySelector('ul');
 const removeButton = document.getElementsByClassName('remove');
 const add = document.getElementById('add')
+const completeButton = document.getElementsByClassName('complete');
 
 add.addEventListener('click', function (event) {
     event.preventDefault();
@@ -28,13 +29,19 @@ function addItemTodo(text) {
     let list = document.getElementById('todo');
     let item = document.createElement('li');
     let remove = document.createElement('button');
+    let complete = document.createElement('button')
     remove.className = 'remove';
+    complete.className = 'complete';
     item.innerText = text;
 
 
     //add event listener remove item
     remove.addEventListener('click', removeItem);
 
+
     list.appendChild(item);
     item.appendChild(remove);
+    item.appendChild(complete);
+
 };
+
